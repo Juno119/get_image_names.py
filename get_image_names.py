7f414ee6170712ee80e12ffb2a20e5961750b213
charnet1019 <charnet1019@163.com>
2017-11-12 12:51:04 +0800

    Update get_image_names.py
    增加获取指定数量的镜像定义

--- get_image_names.py
+++ get_image_names.py
@@ -17,6 +17,8 @@ import traceback
 # 仓库地址
 repo_ip = 'registry'
 repo_port = 5000
+# 不加此值一次无法获取到此值内所有的镜像
+COUNT=3000
 
 def isNotEmpty(s):
     return s and len(s.strip()) > 0
@@ -24,12 +26,12 @@ def isNotEmpty(s):
 def getImagesNames(repo_ip,repo_port):
     docker_images = []
     try:
-        url = "http://" + repo_ip + ":" +str(repo_port) + "/v2/_catalog"
+        url = "http://" + repo_ip + ":" + str(repo_port) + "/v2/_catalog?n=" + str(COUNT)
         res =requests.get(url).content.strip()
         res_dic = json.loads(res)
         images_type = res_dic['repositories']
         for i in images_type:
-            url2 = "http://" + repo_ip + ":" +str(repo_port) +"/v2/" + str(i) + "/tags/list"
+            url2 = "http://" + repo_ip + ":" + str(repo_port) +"/v2/" + str(i) + "/tags/list"
             res2 =requests.get(url2).content.strip()
             res_dic2 = json.loads(res2)
             name = res_dic2['name']
